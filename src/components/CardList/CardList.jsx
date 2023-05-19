1d1b58d2d109f529df6b1435a5279cc535e4dd5d
Dmitrii Liubich <airvaffeforce@yandex.ru>
2023-05-19 22:31:54 +0300

    project completed

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,33 +1,38 @@
 import React, { useEffect } from 'react'
-import {mockObject} from '../../mockData'
-import { useState } from 'react';
+// import {mockObject} from '../../mockData'
+// import { useState } from 'react';
 import './CardList.css';
 import {useSelector, useDispatch} from 'react-redux';
 import { handleBtnColor, handleQuantityCard, fetchCards } from '../../store/cardSlice';
 
 const CardList = () => {
     const cardList = useSelector(state => state.cards.cardList)
+    const {status, error} = useSelector(state => state.cards)
     const dispatch = useDispatch()
     const changeColor = (cardId) => dispatch(handleBtnColor(cardId))
     const changeQuantity = (cardId) => dispatch(handleQuantityCard(cardId))
     
     useEffect(() => {
         if(!cardList.length) {
-            console.log('check')
             dispatch(fetchCards())
         }
     }, [dispatch, cardList.length])
     return (
         <>
-            {cardList.map(object => {
+            { status === 'pending' && <span className="loader"></span>} 
+            { error === 'rejected' && <h1>К сожалению что-то пошло не так</h1>} 
+            { status === 'fulfilled' && cardList.map(object => {
             return(
                     <div className='card' key={object.id} >
                         <button className='card__linkBtn'><a href={object.url} rel="noopener noreferrer">ПЕРЕЙТИ К ИСТОЧНИКУ</a></button>
                         <h2 className='card_title'>{object.title}</h2>
                         <section className='card__section'>{object.description}</section>
-                        <div className='card__picture'>
-                            <img src={object.image} alt=''></img>
-                        </div>
+                        {object.image === 'None'
+                        ? null
+                        : <div className='card__picture'> 
+                             <img src={object.image} alt='' title='место картинки'></img> 
+                        </div> }
+                       
                         <div className='card__btnContainer'>
                             <button 
                             className={'card__heartBtn' + object.liked && 'card__heartBtn card__heartBtn_red'}
@@ -39,9 +44,9 @@ const CardList = () => {
                         </div>
                     </div>
             )
-        })}
+        })
+    }
         </>
     ) 
-
 }
-export default CardList
+export default CardList;
--- src/store/cardSlice.js
+++ src/store/cardSlice.js
@@ -1,25 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // import { mockObject } from "../mockData";
-import { getCards } from "../services";
+// import { getCards } from "../services";
 
 export const fetchCards = createAsyncThunk(
     'cards/fetchCards',
-    async () => {
-        const response = await getCards()
-        console.log(response.news)   
-        return response.news
+    async (_, {rejectWithValue}) => {
+        try {
+            const response = await fetch("https://api.currentsapi.services/v1/latest-news?language=en&apiKey=Fw07-oG8z-92ktSm0nHBes-AydcXTYNydy9nZxBP3xY1gQZT")
+            if(!response.status){
+                throw new Error('Server error')
+            }
+            const data = await response.json();
+            return data.news.slice(0,12)
+        }catch(error) {
+            return rejectWithValue(error.message)
+        }
     }
 )
 const cardSlice =  createSlice({
     name: 'cards',
     initialState:{
         cardList: [],
+        status: null,
+        error: null
     },
     reducers:{
        handleBtnColor:(state,action) => {
-            
-            console.log(action.payload)
-            
             state.cardList = state.cardList.map((obj) => {
                         if(obj.id !== action.payload) {
                             return obj
@@ -30,14 +36,22 @@ const cardSlice =  createSlice({
         },
         handleQuantityCard:(state, action) => {
             state.cardList = state.cardList.filter((obj) => obj.id !== action.payload )
+        }
     },
     extraReducers:(builder) => {
+        builder.addCase(fetchCards.pending,(state) => {
+            state.status = 'pending';
+            state.error = null
+        })
         builder.addCase(fetchCards.fulfilled,(state, action) => {
-            console.log('fffff')
+            state.status = 'fulfilled'
             state.cardList = action.payload
         })
+        builder.addCase(fetchCards.rejected,(state, action) => {
+            state.error = 'rejected';
+            state.status = action.payload
+        })
     }
-}
 })
 export const {handleBtnColor, handleQuantityCard} = cardSlice.actions
 export default cardSlice.reducer
